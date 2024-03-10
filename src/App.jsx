efc106bc86cf134288504dae7b3efeb8f23754b0
Rachkem01 <122974865+Rachkem01@users.noreply.github.com>
2024-03-10 15:21:10 +0100

    Update App.jsx
    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import Portfolio from './portfolio/portfolio'
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <Portfolio></Portfolio>
+    <section>
+        <h1 className="Akube">AKUBE</h1>
+        <div>
+            <h4 className="React">React Front-End Developer</h4>
+            <p className="Rachael"> I am Rachael Babatunde</p>
+            <p>
+                <button className="more">MORE ABOUT ME</button>
+                <button className="hire">HIRE ME</button>
+            </p>
+        
+        </div>
+
+        <div>
+        <p>&copy;{new Date().getFullYear()}This template is made by me</p>
+            
+        </div>
+    </section>
+ )
   )
 }
 
